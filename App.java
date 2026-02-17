05be0c7375b3a16ca9fc6f9084cee6d0643a2a90
Dcaller4E <dcaller4@epcc.edu>
2026-02-17 18:03:28 +0000

    Date update

--- App.java
+++ App.java
@@ -3,13 +3,28 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        System.out.print("Enter age: ");
-        int age = keyboard.nextInt();
+        
+        // Enter a day
+        System.out.print("Enter a day: ");
+        int day = keyboard.nextInt();
+        boolean validDay = ( 1 <= day && day <= 31 );
+        
+        // Enter a month
+        System.out.print("Enter a month: ");
+        int month = keyboard.nextInt();
+        boolean validMonth = ( 1 <= month && month <= 12 );
 
-        if (age < 13 || age > 55) {
-            System.out.println("Discounted ticket!");
-        } else {
-            System.out.println("Regular price.");
+        // check data
+        if (!validDay) {
+            System.out.println("Invalid day!");
+        }
+
+        if (!validMonth) {
+            System.out.println("Invalid month!");
+        }
+
+        if (validDay && validMonth) {
+            System.out.println("Looks like a valid date.");
         }
     }
 }
